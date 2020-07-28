5f488014ac839f3dc824e31f1b8bc0c8dd066e71
KANIN SRIJUNDORN <kaninsrijundorn@MacBook-Pro.local>
2020-07-28 23:11:24 +0700

    first

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,92 +1,129 @@
 import React from 'react';
-import { Form, Input, Button, Row, Col, Divider } from 'antd';
+import { Form, Input, Button, Row, Col, Divider, notification } from 'antd';
 import Title from 'antd/lib/typography/Title';
+import axios from '../../config/axios';
+import { withRouter } from 'react-router-dom';
 
 const layout = {
-    labelCol: { xs: 24, sm: 7, md: 6, lg: 6, xl: 5, xxl: 4 },
-    wrapperCol: { xs: 24, sm: 17, md: 18, lg: 18, xl: 19, xxl: 20 },
+  labelCol: { xs: 24, sm: 7, md: 6, lg: 6, xl: 5, xxl: 4 },
+  wrapperCol: { xs: 24, sm: 17, md: 18, lg: 18, xl: 19, xxl: 20 },
 };
-export default function Register() {
+function Register(props) {
+  console.log(props);
 
-    const onFinish = values => {
-        console.log('Received values of form: ', values);
-    };
+  const onFinish = (values) => {
+    const { email, password, name } = values;
+    axios
+      .post('/users/register', {
+        username: email,
+        password,
+        name,
+      })
+      .then((res) => {
+        notification.success({
+          message:
+            'สมัครเสร็จเรียบร้อยแล้วนะจ๊ะจะบอกให้ถ้ายังไม่รู้ก็รู้ไว้ซะนะจ๊ะ อิอิ',
+        });
+        props.history.push('/login');
+      })
+      .catch((err) => {
+        notification.error({
+          message: 'บางอย่าง Wrong',
+        });
+      });
+  };
 
-    return (
-        <Row justify="center" >
-            <Col xs={23} sm={23} md={23} lg={14} xl={14} xxl={12}>
-                <div
-                    className="Form"
-                >
-                    <Row justify="center">
-                        <Title level={2} className="Title">
-                            Register
-                        </Title>
-                    </Row>
-                    <Divider className="Divider" />
-                    <Form
-                        {...layout}
-                        onFinish={onFinish}
-                        style={{ width: "100%" }}
-                    >
-                        <Form.Item
-                            name="email"
-                            label="E-mail"
-                            rules={[
-                                {
-                                    type: 'email',
-                                    message: 'The input is not valid E-mail!',
-                                },
-                                {
-                                    required: true,
-                                    message: 'Please input your E-mail!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
+  return (
+    <Row justify='center'>
+      <Col xs={23} sm={23} md={23} lg={14} xl={14} xxl={12}>
+        <div className='Form'>
+          <Row justify='center'>
+            <Title level={2} className='Title'>
+              Register
+            </Title>
+          </Row>
+          <Divider className='Divider' />
+          <Form {...layout} onFinish={onFinish} style={{ width: '100%' }}>
+            <Form.Item
+              name='email'
+              label='E-mail'
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-                        <Form.Item
-                            name="password"
-                            label="Password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your password!',
-                                },
-                            ]}
-                            hasFeedback
-                        >
-                            <Input.Password />
-                        </Form.Item>
+            <Form.Item
+              name='password'
+              label='Password'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password />
+            </Form.Item>
 
-                        <Form.Item
-                            name="confirm"
-                            label="Confirm Password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please confirm your password!',
-                                },
-                            ]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
+            <Form.Item
+              hasFeedback
+              name='confirm'
+              label='Confirm Password'
+              dependencies={['password']}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                function (form) {
+                  const validator = function (rule, value) {
+                    if (!value || form.getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      'ยินยันพาสเวิร์ด ต้องตรงกับ พาสเวิร์ด'
+                    );
+                  };
 
-                        <Form.Item
-                            name="nickname"
-                            label={<span>Nickname&nbsp;</span>}
-                            rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
-                        >
-                            <Input />
-                        </Form.Item>
+                  return { validator };
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-                            <Button className="Button" type="primary" htmlType="submit">
-                                Register
-                            </Button>
-                    </Form>
-                </div>
-            </Col>
-        </Row>
-    );
+            <Form.Item
+              name='name'
+              label={<span>Nickname&nbsp;</span>}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your nickname!',
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Button className='Button' type='primary' htmlType='submit'>
+              Register
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
+  );
 }
+
+export default withRouter(Register);
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = 'http://localhost:8000';
+
+// ก่อนที่จะส่ง request ต้องมาทำอันนี้ก่อน
+axios.interceptors.request.use();
 
 export default axios;
